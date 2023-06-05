c0c0e61770e48e9c67bf0737e790da9dd36c606f
VAHC <victor10alha@gmail.com>
2023-06-05 15:39:24 -0300

    update commit six

--- api/src/app.js
+++ api/src/app.js
@@ -4,10 +4,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const passport = require('passport');
-const session = require('cookie-session');
-//const session = require('express-session');
+const cookieSession = require('cookie-session');
 const cors = require('cors');
-
 
 const server = express();
 
@@ -16,7 +14,7 @@ const corsOptions = {
   origin: 'http://127.0.0.1:5173',
   credentials: true,
 };
-server.use(express.static('uploads'))// que carpeta es visible, es la raiz
+server.use(express.static('uploads')) // Carpeta visible, es la raíz
 
 server.use(cors(corsOptions));
 server.name = 'API';
@@ -25,40 +23,36 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 //server.set('view engine', 'ejs'); // Eliminar cuando esté el front
 
-// Configuración de express-session
-server.use(
-  session({
-    secret: 'secreto',
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+const sessionConfig = {
+  name: 'session',
+  secret: 'secreto',
+  maxAge: 24 * 60 * 60 * 1000, // Duración de la sesión en milisegundos (aquí, 24 horas)
+};
+
+server.use(cookieSession(sessionConfig));
 
 server.use(passport.initialize());
-server.use(passport.session()); // aca es donde se deberia crear la cookie
-
-// En caso de ser usado localmente el proyecto, se debe descomentar la linea 44 y comentar la linea 43
-// 'Access-Control-Allow-Origin'
+server.use(passport.session()); // Aquí se crea la cookie de sesión
 
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'https://rayuela-book-club.vercel.app') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Origin', 'https://deploy-six-amber.vercel.app/')
-  // res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
-})
+  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
-server.use('/', routes)
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500
-  const message = err.message || err
-  console.error(err)
-  res.status(status).send(message)
-})
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
-module.exports = server
+module.exports = server;
