ef4de64e7fb613008f11880079efc55fc910b69f
VAHC <victor10alha@gmail.com>
2023-06-05 14:51:37 -0300

    update commit fix

--- api/rutas.js
+++ api/rutas.js
@@ -1,4 +1,7 @@
-export const URL_Railway = 'https://deploy-production-dccf.up.railway.app'
+module.exports.URL_Railway = 'https://deploy-production-dccf.up.railway.app';
+
+
+//const URL_Railway = 'https://deploy-production-dccf.up.railway.app'
 
 //  const URL_Railway = 'http://localhost:3001'
 //  const URL_Vercel = 'http://127.0.0.1:5173'
