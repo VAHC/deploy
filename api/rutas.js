ae53b6ff2a72ac7c1d45b8af4c33db3f47a3eb90
VAHC <victor10alha@gmail.com>
2023-06-06 08:36:12 -0300

    update one

--- api/rutas.js
+++ api/rutas.js
@@ -4,7 +4,7 @@
 // export const URL_Vercel = 'http://127.0.0.1:5173'
 
 const URL_Railway = 'https://deploy-production-dccf.up.railway.app'
-const URL_Vercel = ' https://deploy-six-amber.vercel.app'
+const URL_Vercel = 'https://deploy-six-amber.vercel.app'
 
 // Dependiendo del entorno en donde se este trabajando, se descomenta
 // una o la otra.
